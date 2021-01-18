fa2338ff56cbe8e18797063fbc3c996a3573dfa5
Marlon <marlon.belohd@gmail.com>
2021-01-18 17:43:58 -0300

    simple queries with @Query

--- src/main/java/com/marlonmarques/workshopspringbootmongo/repository/PostRepository.java
+++ src/main/java/com/marlonmarques/workshopspringbootmongo/repository/PostRepository.java
@@ -3,12 +3,16 @@ package com.marlonmarques.workshopspringbootmongo.repository;
 import com.marlonmarques.workshopspringbootmongo.domain.Post;
 import com.marlonmarques.workshopspringbootmongo.domain.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+    List<Post> findByTitle(String text);
 
     List<Post> findByTitleContainingIgnoreCase(String text);
 }
