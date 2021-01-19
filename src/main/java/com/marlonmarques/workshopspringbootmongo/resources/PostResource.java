75142621221f70d30a80252c9a3d1a74fbe929be
Marlon <marlon.belohd@gmail.com>
2021-01-19 09:19:45 -0300

    consultations with various criteria

--- src/main/java/com/marlonmarques/workshopspringbootmongo/resources/PostResource.java
+++ src/main/java/com/marlonmarques/workshopspringbootmongo/resources/PostResource.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -24,10 +25,15 @@ public class PostResource {
         return ResponseEntity.ok().body(obj);
     }
 
-    @RequestMapping(value = "/titlesearch", method = RequestMethod.GET)
-    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+    @RequestMapping(value = "/fullsearch", method = RequestMethod.GET)
+    public ResponseEntity<List<Post>> fullSearch(
+            @RequestParam(value = "text", defaultValue = "") String text,
+            @RequestParam(value = "minDate", defaultValue = "") String minDate,
+            @RequestParam(value = "maxDate", defaultValue = "") String maxDate) {
         text = URL.decodeParam(text);
-        List<Post> list = service.findByTitle(text);
+        Date min = URL.convertDate(minDate, new Date(0L));
+        Date max = URL.convertDate(maxDate, new Date());
+        List<Post> list = service.fullSearch(text, min, max);
         return ResponseEntity.ok().body(list);
     }
 }
--- src/main/java/com/marlonmarques/workshopspringbootmongo/services/PostService.java
+++ src/main/java/com/marlonmarques/workshopspringbootmongo/services/PostService.java
@@ -8,6 +8,7 @@ import com.marlonmarques.workshopspringbootmongo.services.exception.ObjectNotFou
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -27,5 +28,10 @@ public class PostService {
 
     public List<Post> findByTitle(String text) {
         return repository.findByTitle(text);
+    }
+
+    public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+        maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+        return repository.fullSearch(text, minDate, maxDate);
     }
 }
